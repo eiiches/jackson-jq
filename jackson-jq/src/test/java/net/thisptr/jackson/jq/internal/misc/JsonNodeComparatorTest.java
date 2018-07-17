3800361caff08ce32d1907638374ffe3fc890d36
Eiichi Sato <sato.eiichi@gmail.com>
2018-07-17 22:06:08 +0900

    starting a slow migration to junit 5

--- jackson-jq/src/test/java/net/thisptr/jackson/jq/internal/misc/JsonNodeComparatorTest.java
+++ jackson-jq/src/test/java/net/thisptr/jackson/jq/internal/misc/JsonNodeComparatorTest.java
@@ -1,7 +1,7 @@
 package net.thisptr.jackson.jq.internal.misc;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -11,7 +11,7 @@ import java.util.List;
 
 import net.thisptr.jackson.jq.internal.misc.JsonNodeComparator;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
