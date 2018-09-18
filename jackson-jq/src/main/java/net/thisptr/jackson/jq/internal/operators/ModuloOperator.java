1fa2043f70e29b604151010db98459a03d19dfdc
Eiichi Sato <sato.eiichi@gmail.com>
2018-09-19 00:53:05 +0900

    operator%: support floating-point numbers

--- jackson-jq/src/main/java/net/thisptr/jackson/jq/internal/operators/ModuloOperator.java
+++ jackson-jq/src/main/java/net/thisptr/jackson/jq/internal/operators/ModuloOperator.java
@@ -4,19 +4,20 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import net.thisptr.jackson.jq.exception.JsonQueryException;
+import net.thisptr.jackson.jq.exception.JsonQueryTypeException;
 import net.thisptr.jackson.jq.internal.misc.JsonNodeUtils;
 
 public class ModuloOperator implements BinaryOperator {
 	@Override
 	public JsonNode apply(ObjectMapper mapper, JsonNode lhs, JsonNode rhs) throws JsonQueryException {
-		if (lhs.isIntegralNumber() && rhs.isIntegralNumber()) {
+		if (lhs.isNumber() && rhs.isNumber()) {
 			final long divisor = rhs.asLong();
 			final long dividend = lhs.asLong();
 			if (divisor == 0L)
 				throw JsonQueryException.format("number (%s) and number (%s) cannot be divided (remainder) because the divisor is zero", dividend, divisor);
 			return JsonNodeUtils.asNumericNode(dividend % divisor);
 		} else {
-			throw new JsonQueryException("to calculate module, both sides must be a integer");
+			throw new JsonQueryTypeException(lhs, rhs, "cannot be divided (remainder)");
 		}
 	}
 
