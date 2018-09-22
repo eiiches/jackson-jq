1f618d610e023679f41287232fe5696bd0b7d74c
Eiichi Sato <sato.eiichi@gmail.com>
2018-09-22 22:21:18 +0900

    to_entries/0: support array input

--- jackson-jq/src/main/java/net/thisptr/jackson/jq/internal/functions/ToEntriesFunction.java
+++ jackson-jq/src/main/java/net/thisptr/jackson/jq/internal/functions/ToEntriesFunction.java
@@ -6,7 +6,7 @@ import java.util.Map.Entry;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ArrayNode;
-import com.fasterxml.jackson.databind.node.JsonNodeType;
+import com.fasterxml.jackson.databind.node.IntNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.fasterxml.jackson.databind.node.TextNode;
 
@@ -16,23 +16,35 @@ import net.thisptr.jackson.jq.Output;
 import net.thisptr.jackson.jq.Scope;
 import net.thisptr.jackson.jq.Version;
 import net.thisptr.jackson.jq.exception.JsonQueryException;
+import net.thisptr.jackson.jq.exception.JsonQueryTypeException;
 import net.thisptr.jackson.jq.internal.BuiltinFunction;
-import net.thisptr.jackson.jq.internal.misc.Preconditions;
 
 @BuiltinFunction("to_entries/0")
 public class ToEntriesFunction implements Function {
 	@Override
 	public void apply(final Scope scope, final List<Expression> args, final JsonNode in, final Output output, final Version version) throws JsonQueryException {
-		Preconditions.checkInputType("to_entries", in, JsonNodeType.OBJECT);
-
 		final ArrayNode out = scope.getObjectMapper().createArrayNode();
-		final Iterator<Entry<String, JsonNode>> iter = in.fields();
-		while (iter.hasNext()) {
-			final Entry<String, JsonNode> entry = iter.next();
-			final ObjectNode entryNode = scope.getObjectMapper().createObjectNode();
-			entryNode.set("key", new TextNode(entry.getKey()));
-			entryNode.set("value", entry.getValue());
-			out.add(entryNode);
+
+		if (in.isObject()) {
+			final Iterator<Entry<String, JsonNode>> iter = in.fields();
+			while (iter.hasNext()) {
+				final Entry<String, JsonNode> entry = iter.next();
+				final ObjectNode entryNode = scope.getObjectMapper().createObjectNode();
+				entryNode.set("key", new TextNode(entry.getKey()));
+				entryNode.set("value", entry.getValue());
+				out.add(entryNode);
+			}
+		} else if (in.isArray()) {
+			final Iterator<JsonNode> iter = in.iterator();
+			for (int i = 0; iter.hasNext(); ++i) {
+				final JsonNode value = iter.next();
+				final ObjectNode entryNode = scope.getObjectMapper().createObjectNode();
+				entryNode.set("key", IntNode.valueOf(i));
+				entryNode.set("value", value);
+				out.add(entryNode);
+			}
+		} else {
+			throw JsonQueryTypeException.format("%s has no keys", in);
 		}
 
 		output.emit(out);
