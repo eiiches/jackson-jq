16fb54b527457c4c0a3bdfba2472db4cb80ff925
Eiichi Sato <sato.eiichi@gmail.com>
2018-09-25 14:50:17 +0900

    fromjson/0: raise an error when input is empty or has trailing data

--- jackson-jq/src/main/java/net/thisptr/jackson/jq/internal/functions/FromJsonFunction.java
+++ jackson-jq/src/main/java/net/thisptr/jackson/jq/internal/functions/FromJsonFunction.java
@@ -3,8 +3,8 @@ package net.thisptr.jackson.jq.internal.functions;
 import java.io.IOException;
 import java.util.List;
 
+import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.node.JsonNodeType;
 
 import net.thisptr.jackson.jq.Expression;
 import net.thisptr.jackson.jq.Function;
@@ -12,19 +12,28 @@ import net.thisptr.jackson.jq.Output;
 import net.thisptr.jackson.jq.Scope;
 import net.thisptr.jackson.jq.Version;
 import net.thisptr.jackson.jq.exception.JsonQueryException;
+import net.thisptr.jackson.jq.exception.JsonQueryTypeException;
 import net.thisptr.jackson.jq.internal.BuiltinFunction;
-import net.thisptr.jackson.jq.internal.misc.Preconditions;
 
 @BuiltinFunction("fromjson/0")
 public class FromJsonFunction implements Function {
 	@Override
 	public void apply(final Scope scope, final List<Expression> args, final JsonNode in, final Output output, final Version version) throws JsonQueryException {
-		Preconditions.checkInputType("fromjson", in, JsonNodeType.STRING);
+		if (!in.isTextual())
+			throw new JsonQueryTypeException("%s only strings can be parsed", in);
 
-		try {
-			output.emit(scope.getObjectMapper().readTree(in.asText()));
-		} catch (IOException e) {
-			throw new JsonQueryException(e);
+		final JsonNode tree;
+		try (final JsonParser parser = scope.getObjectMapper().getFactory().createParser(in.asText())) {
+			tree = parser.readValueAsTree();
+			if (tree == null)
+				throw JsonQueryException.format("failed to parse %s as json; empty", in);
+			if (parser.nextToken() != null)
+				throw JsonQueryException.format("failed to parse %s as json; trailing data", in);
+		} catch (final JsonQueryException e) {
+			throw e;
+		} catch (final IOException e) {
+			throw JsonQueryException.format("failed to parse %s as json", in);
 		}
+		output.emit(tree);
 	}
 }
