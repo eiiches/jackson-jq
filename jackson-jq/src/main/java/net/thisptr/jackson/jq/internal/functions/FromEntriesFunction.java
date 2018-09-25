fa476a4c6cb43877c1c53c0752eed41a19aa0dd2
Eiichi Sato <sato.eiichi@gmail.com>
2018-09-26 00:00:53 +0900

    from_entries/0: allow object as input and fix error messages

--- jackson-jq/src/main/java/net/thisptr/jackson/jq/internal/functions/FromEntriesFunction.java
+++ jackson-jq/src/main/java/net/thisptr/jackson/jq/internal/functions/FromEntriesFunction.java
@@ -3,7 +3,6 @@ package net.thisptr.jackson.jq.internal.functions;
 import java.util.List;
 
 import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.node.JsonNodeType;
 import com.fasterxml.jackson.databind.node.NullNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
@@ -13,17 +12,21 @@ import net.thisptr.jackson.jq.Output;
 import net.thisptr.jackson.jq.Scope;
 import net.thisptr.jackson.jq.Version;
 import net.thisptr.jackson.jq.exception.JsonQueryException;
+import net.thisptr.jackson.jq.exception.JsonQueryTypeException;
 import net.thisptr.jackson.jq.internal.BuiltinFunction;
-import net.thisptr.jackson.jq.internal.misc.Preconditions;
 
 @BuiltinFunction("from_entries/0")
 public class FromEntriesFunction implements Function {
 	@Override
 	public void apply(final Scope scope, final List<Expression> args, final JsonNode in, final Output output, final Version version) throws JsonQueryException {
-		Preconditions.checkInputArrayType("from_entries", in, JsonNodeType.OBJECT);
+		if (!in.isArray() && !in.isObject())
+			throw new JsonQueryTypeException("Cannot iterate over %s", in);
 
 		final ObjectNode out = scope.getObjectMapper().createObjectNode();
 		for (final JsonNode entry : in) {
+			if (!entry.isObject())
+				throw new JsonQueryTypeException("Cannot index %s with string \"key\"", entry.getNodeType().toString().toLowerCase());
+
 			JsonNode key = entry.get("key");
 			if (key == null)
 				key = entry.get("Key");
@@ -32,10 +35,12 @@ public class FromEntriesFunction implements Function {
 			if (key == null)
 				key = entry.get("Name");
 			if (key == null || !key.isTextual())
-				throw new JsonQueryException("input elements to from_entries() must be object and have 'key', 'Key' or 'Name' field");
+				throw new JsonQueryTypeException("Cannot use %s as object key", key == null ? NullNode.getInstance() : key);
+
 			JsonNode value = entry.get("value");
 			if (value == null)
 				value = entry.get("Value");
+
 			out.set(key.asText(), value == null ? NullNode.getInstance() : value);
 		}
 
