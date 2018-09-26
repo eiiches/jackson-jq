ecb4e4f97adccf06a9aa88b163eb6eaab157081f
Eiichi Sato <sato.eiichi@gmail.com>
2018-09-27 07:51:45 +0900

    reverse/0: zero-length inputs such as {}, "", 0 and null should emit [] without errors

--- jackson-jq/src/main/java/net/thisptr/jackson/jq/internal/functions/ReverseFunction.java
+++ jackson-jq/src/main/java/net/thisptr/jackson/jq/internal/functions/ReverseFunction.java
@@ -1,11 +1,9 @@
 package net.thisptr.jackson.jq.internal.functions;
 
-import java.util.Collections;
 import java.util.List;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ArrayNode;
-import com.fasterxml.jackson.databind.node.JsonNodeType;
 
 import net.thisptr.jackson.jq.Expression;
 import net.thisptr.jackson.jq.Function;
@@ -13,18 +11,52 @@ import net.thisptr.jackson.jq.Output;
 import net.thisptr.jackson.jq.Scope;
 import net.thisptr.jackson.jq.Version;
 import net.thisptr.jackson.jq.exception.JsonQueryException;
+import net.thisptr.jackson.jq.exception.JsonQueryTypeException;
 import net.thisptr.jackson.jq.internal.BuiltinFunction;
-import net.thisptr.jackson.jq.internal.misc.JsonNodeUtils;
-import net.thisptr.jackson.jq.internal.misc.Preconditions;
 
 @BuiltinFunction("reverse/0")
 public class ReverseFunction implements Function {
 	@Override
 	public void apply(final Scope scope, final List<Expression> args, final JsonNode in, final Output output, final Version version) throws JsonQueryException {
-		Preconditions.checkInputType("reverse", in, JsonNodeType.ARRAY);
+		final ArrayNode out = scope.getObjectMapper().createArrayNode();
 
-		final List<JsonNode> items = JsonNodeUtils.asArrayList((ArrayNode) in);
-		Collections.reverse(items);
-		output.emit(JsonNodeUtils.asArrayNode(scope.getObjectMapper(), items));
+		if (in.isNull()) {
+			output.emit(out);
+			return;
+		}
+		if (in.isArray()) {
+			for (int i = in.size() - 1; i >= 0; --i)
+				out.add(in.get(i));
+			output.emit(out);
+			return;
+		}
+
+		// below are to emulate jq behavior
+
+		if (in.isTextual()) {
+			if (in.asText().isEmpty()) {
+				output.emit(out);
+				return;
+			}
+			throw JsonQueryException.format("Cannot index %s with number", in.getNodeType());
+		}
+		if (in.isNumber()) {
+			if (in.asDouble() == 0.0) {
+				output.emit(out);
+				return;
+			}
+			throw JsonQueryException.format("Cannot index %s with number", in.getNodeType());
+		}
+		if (in.isObject()) {
+			if (in.size() == 0) {
+				output.emit(out);
+				return;
+			}
+			throw JsonQueryException.format("Cannot index %s with number", in.getNodeType());
+		}
+		if (in.isBoolean()) {
+			throw new JsonQueryTypeException("%s has no length", in);
+		}
+		throw new JsonQueryTypeException("%s cannot be reversed", in);
 	}
 }
