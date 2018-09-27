f275bc11581ef4ab29b813f342c9961330fe6a46
Eiichi Sato <sato.eiichi@gmail.com>
2018-09-27 16:22:40 +0900

    range/{1,2,3}: support non-numerical range and fix argument evaluation order

--- jackson-jq/src/main/java/net/thisptr/jackson/jq/internal/functions/RangeFunction.java
+++ jackson-jq/src/main/java/net/thisptr/jackson/jq/internal/functions/RangeFunction.java
@@ -3,44 +3,82 @@ package net.thisptr.jackson.jq.internal.functions;
 import java.util.List;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.IntNode;
 
 import net.thisptr.jackson.jq.Expression;
 import net.thisptr.jackson.jq.Function;
 import net.thisptr.jackson.jq.Output;
 import net.thisptr.jackson.jq.Scope;
 import net.thisptr.jackson.jq.Version;
+import net.thisptr.jackson.jq.Versions;
 import net.thisptr.jackson.jq.exception.JsonQueryException;
+import net.thisptr.jackson.jq.exception.JsonQueryTypeException;
 import net.thisptr.jackson.jq.internal.BuiltinFunction;
+import net.thisptr.jackson.jq.internal.misc.JsonNodeComparator;
 import net.thisptr.jackson.jq.internal.misc.JsonNodeUtils;
-import net.thisptr.jackson.jq.internal.misc.JsonQueryUtils;
+import net.thisptr.jackson.jq.internal.operators.PlusOperator;
 
-@BuiltinFunction("range/3")
+@BuiltinFunction({ "range/1", "range/2", "range/3" })
 public class RangeFunction implements Function {
 
 	@Override
 	public void apply(final Scope scope, final List<Expression> args, final JsonNode in, final Output output, final Version version) throws JsonQueryException {
-		final List<JsonNode> beginTuple = JsonQueryUtils.applyToArrayList(args.get(0), scope, in);
-		final List<JsonNode> endTuple = JsonQueryUtils.applyToArrayList(args.get(1), scope, in);
-		final List<JsonNode> incTuple = JsonQueryUtils.applyToArrayList(args.get(2), scope, in);
-
-		for (final JsonNode beginNode : beginTuple) {
-			for (final JsonNode endNode : endTuple) {
-				for (final JsonNode incNode : incTuple) {
-					if (!beginNode.isNumber() || !endNode.isNumber() || !incNode.isNumber())
-						throw new JsonQueryException("arguments of range() must be NUMBER");
-					final double begin = beginNode.asDouble();
-					final double end = endNode.asDouble();
-					final double inc = incNode.asDouble();
-
-					if (inc > 0) {
-						for (double i = begin; i < end; i += inc)
-							output.emit(JsonNodeUtils.asNumericNode(i));
-					} else if (inc < 0) {
-						for (double i = begin; i > end; i += inc)
-							output.emit(JsonNodeUtils.asNumericNode(i));
-					}
+		if (args.size() == 1) {
+			args.get(0).apply(scope, in, (end) -> {
+				range1(output, end);
+			});
+		} else if (args.size() == 2) {
+			args.get(0).apply(scope, in, (start) -> {
+				if (version.compareTo(Versions.JQ_1_5) <= 0) {
+					final JsonNode[] cur = new JsonNode[] { start }; // only reset when start changes [v1.5]
+					args.get(1).apply(scope, in, (end) -> {
+						cur[0] = range2(output, cur[0], end);
+					});
+				} else {
+					args.get(1).apply(scope, in, (end) -> {
+						range2(output, start, end);
+					});
 				}
-			}
+			});
+		} else {
+			args.get(0).apply(scope, in, (start) -> {
+				args.get(1).apply(scope, in, (end) -> {
+					args.get(2).apply(scope, in, (incr) -> {
+						range3(output, start, end, incr);
+					});
+				});
+			});
+		}
+	}
+
+	private static void range1(final Output output, final JsonNode end) throws JsonQueryException {
+		range2(output, IntNode.valueOf(0), end);
+	}
+
+	private static JsonNode range2(final Output output, final JsonNode start, final JsonNode end) throws JsonQueryException {
+		if (!start.isNumber() || !end.isNumber())
+			throw new JsonQueryTypeException("Range bounds must be numeric");
+		final double _start = start.asDouble();
+		final double _end = end.asDouble();
+		double i;
+		for (i = _start; i < _end; i += 1)
+			output.emit(JsonNodeUtils.asNumericNode(i));
+		return JsonNodeUtils.asNumericNode(i);
+	}
+
+	private static final JsonNodeComparator COMPARATOR = JsonNodeComparator.getInstance();
+	private static final PlusOperator OPERATOR = new PlusOperator();
+	private static final ObjectMapper MAPPER = new ObjectMapper();
+
+	private static void range3(final Output output, final JsonNode start, final JsonNode end, final JsonNode incr) throws JsonQueryException {
+		final int dir = Integer.signum(COMPARATOR.compare(IntNode.valueOf(0), incr));
+		if (dir == 0)
+			return;
+		JsonNode cur = start;
+		while (Integer.signum(COMPARATOR.compare(cur, end)) == dir) {
+			output.emit(cur);
+			cur = OPERATOR.apply(MAPPER, cur, incr);
 		}
 	}
 }
