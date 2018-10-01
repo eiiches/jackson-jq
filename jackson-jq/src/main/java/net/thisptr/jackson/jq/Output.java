2f7df3ceb33d2b9b415e41eb7e5d08742b24965e
Eiichi Sato <sato.eiichi@gmail.com>
2018-10-02 00:05:12 +0900

    core: interface Output now extends PathOutput

--- jackson-jq/src/main/java/net/thisptr/jackson/jq/Output.java
+++ jackson-jq/src/main/java/net/thisptr/jackson/jq/Output.java
@@ -3,8 +3,14 @@ package net.thisptr.jackson.jq;
 import com.fasterxml.jackson.databind.JsonNode;
 
 import net.thisptr.jackson.jq.exception.JsonQueryException;
+import net.thisptr.jackson.jq.path.Path;
 
-public interface Output {
+public interface Output extends PathOutput {
 
 	void emit(JsonNode out) throws JsonQueryException;
+
+	@Override
+	default void emit(final JsonNode out, final Path opath) throws JsonQueryException {
+		emit(out);
+	}
 }
