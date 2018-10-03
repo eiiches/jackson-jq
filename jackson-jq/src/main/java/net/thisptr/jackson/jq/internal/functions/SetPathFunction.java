b59fcdcaba118bd245de4606a4975431475734f3
Eiichi Sato <sato.eiichi@gmail.com>
2018-10-04 02:18:57 +0900

    setpath/2: fix to evaluate its 2nd argument first

--- jackson-jq/src/main/java/net/thisptr/jackson/jq/internal/functions/SetPathFunction.java
+++ jackson-jq/src/main/java/net/thisptr/jackson/jq/internal/functions/SetPathFunction.java
@@ -19,8 +19,8 @@ public class SetPathFunction implements Function {
 
 	@Override
 	public void apply(final Scope scope, final List<Expression> args, final JsonNode in, final Path inpath, final PathOutput output, final Version version) throws JsonQueryException {
-		args.get(0).apply(scope, in, (pathnode) -> {
-			args.get(1).apply(scope, in, (newvalnode) -> {
+		args.get(1).apply(scope, in, (newvalnode) -> {
+			args.get(0).apply(scope, in, (pathnode) -> {
 				final Path path = PathUtils.toPath(pathnode);
 				final JsonNode out = path.mutate(in, (dummy) -> newvalnode);
 				output.emit(out, path);
