675e5996131d4cd88270b6d83144c897635ac7f8
Eiichi Sato <sato.eiichi@gmail.com>
2019-03-29 20:42:31 +0900

    core: add JsonQueryUndefinedBehaviorException

---
+++ jackson-jq/src/main/java/net/thisptr/jackson/jq/exception/JsonQueryUndefinedBehaviorException.java
@@ -0,0 +1,9 @@
+package net.thisptr.jackson.jq.exception;
+
+public class JsonQueryUndefinedBehaviorException extends JsonQueryException {
+	private static final long serialVersionUID = 6910999258451981582L;
+
+	public JsonQueryUndefinedBehaviorException(final String msg) {
+		super(msg);
+	}
+}
