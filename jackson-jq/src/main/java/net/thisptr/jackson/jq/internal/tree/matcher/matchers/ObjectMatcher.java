beeb74496bff12ca04acdd0f59b7ada26d0e02bb
Eiichi Sato <sato.eiichi@gmail.com>
2021-08-08 07:54:33 +0900

    core: support {$x} and {$x:...} pattern match syntax

--- jackson-jq/src/main/java/net/thisptr/jackson/jq/internal/tree/matcher/matchers/ObjectMatcher.java
+++ jackson-jq/src/main/java/net/thisptr/jackson/jq/internal/tree/matcher/matchers/ObjectMatcher.java
@@ -12,15 +12,59 @@ import net.thisptr.jackson.jq.exception.JsonQueryException;
 import net.thisptr.jackson.jq.exception.JsonQueryTypeException;
 import net.thisptr.jackson.jq.internal.misc.Functional;
 import net.thisptr.jackson.jq.internal.misc.Pair;
+import net.thisptr.jackson.jq.internal.tree.literal.StringLiteral;
 import net.thisptr.jackson.jq.internal.tree.matcher.PatternMatcher;
 import net.thisptr.jackson.jq.path.ObjectFieldPath;
 import net.thisptr.jackson.jq.path.Path;
 
 public class ObjectMatcher implements PatternMatcher {
-	private List<Pair<Expression, PatternMatcher>> matchers;
+	private List<FieldMatcher> matchers;
 
-	public ObjectMatcher(final List<Pair<Expression, PatternMatcher>> matchers) {
+	public ObjectMatcher(final List<FieldMatcher> matchers) {
 		this.matchers = matchers;
+	}
+
+	public static class FieldMatcher {
+		// e.g.
+		// {$x} : dollar = true, name = "x", matcher = null
+		// {$x: [$a]} : dollar = true, name = "x", matcher = [$a]
+		// {x: [$a]} : dollar = false, name = "x", matcher = [$a]
+
+		private final boolean dollar;
+		private final Expression name;
+		private final PatternMatcher matcher;
+
+		public FieldMatcher(final boolean dollar, final Expression name, final PatternMatcher matcher) {
+			if (dollar && !(name instanceof StringLiteral))
+				throw new IllegalArgumentException("BUG: name must be instance of StringLiteral when dollar = true");
+			if (!dollar && matcher == null)
+				throw new IllegalArgumentException("BUG: matcher must not be null when dollar = false");
+			this.dollar = dollar;
+			this.name = name;
+			this.matcher = matcher;
+		}
+
+		@Override
+		public String toString() {
+			final StringBuilder sb = new StringBuilder();
+			if (dollar) {
+				sb.append("$");
+				sb.append(((StringLiteral) name).value().asText());
+			} else {
+				sb.append(name);
+			}
+			if (matcher != null) {
+				sb.append(": ");
+				sb.append(matcher);
+			}
+			return sb.toString();
+		}
+
+		public PatternMatcher matcher() {
+			if (matcher == null)
+				return new ValueMatcher(((StringLiteral) name).value().asText());
+			return matcher;
+		}
 	}
 
 	private void recursive(final Scope scope, final JsonNode in, final Functional.Consumer<List<Pair<String, JsonNode>>> out, final Stack<Pair<String, JsonNode>> accumulate, int index) throws JsonQueryException {
@@ -29,18 +73,17 @@ public class ObjectMatcher implements PatternMatcher {
 			return;
 		}
 
-		final Pair<Expression, PatternMatcher> kvexpr = matchers.get(index);
-		final Expression keyexpr = kvexpr._1;
-		final PatternMatcher matcher = kvexpr._2;
-
-		keyexpr.apply(scope, in, (key) -> {
+		final FieldMatcher fmatcher = matchers.get(index);
+		fmatcher.name.apply(scope, in, (key) -> {
 			if (!key.isTextual())
 				throw new JsonQueryTypeException("Cannot index %s with %s", in.getNodeType(), key.getNodeType());
 
 			final JsonNode value = in.get(key.asText());
 
 			final int size = accumulate.size();
-			matcher.match(scope, value != null ? value : NullNode.getInstance(), (match) -> {
+			if (fmatcher.dollar)
+				accumulate.push(Pair.of(key.asText(), value));
+			fmatcher.matcher().match(scope, value != null ? value : NullNode.getInstance(), (match) -> {
 				recursive(scope, in, out, accumulate, index + 1);
 			}, accumulate);
 			accumulate.setSize(size);
@@ -53,11 +96,8 @@ public class ObjectMatcher implements PatternMatcher {
 			return;
 		}
 
-		final Pair<Expression, PatternMatcher> kvexpr = matchers.get(index);
-		final Expression keyexpr = kvexpr._1;
-		final PatternMatcher matcher = kvexpr._2;
-
-		keyexpr.apply(scope, in, (key) -> {
+		final FieldMatcher fmatcher = matchers.get(index);
+		fmatcher.name.apply(scope, in, (key) -> {
 			if (!key.isTextual())
 				throw new JsonQueryTypeException("Cannot index %s with %s", in.getNodeType(), key.getNodeType());
 
@@ -65,7 +105,9 @@ public class ObjectMatcher implements PatternMatcher {
 			final Path valuepath = ObjectFieldPath.chainIfNotNull(inpath, key.asText());
 
 			final int size = accumulate.size();
-			matcher.matchWithPath(scope, value != null ? value : NullNode.getInstance(), valuepath, (match) -> {
+			if (fmatcher.dollar)
+				accumulate.push(new MatchWithPath(key.asText(), value, valuepath));
+			fmatcher.matcher().matchWithPath(scope, value != null ? value : NullNode.getInstance(), valuepath, (match) -> {
 				recursiveWithPath(scope, in, inpath, output, accumulate, index + 1);
 			}, accumulate);
 			accumulate.setSize(size);
@@ -92,11 +134,9 @@ public class ObjectMatcher implements PatternMatcher {
 	public String toString() {
 		final StringBuilder sb = new StringBuilder("{");
 		String sep = "";
-		for (final Pair<Expression, PatternMatcher> entry : matchers) {
+		for (final FieldMatcher entry : matchers) {
 			sb.append(sep);
-			sb.append(entry._1);
-			sb.append(": ");
-			sb.append(entry._2);
+			sb.append(entry.toString());
 			sep = ", ";
 		}
 		sb.append("}");
