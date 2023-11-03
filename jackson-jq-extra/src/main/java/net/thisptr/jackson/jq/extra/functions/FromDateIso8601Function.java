48acdbf1e57076e7c37b62a33007e99097f4f54c
Nikhil Unni <nikhil.unni@cortex.io>
2023-11-03 16:14:40 -0700

    more fixes

--- jackson-jq-extra/src/main/java/net/thisptr/jackson/jq/extra/functions/FromDateIso8601Function.java
+++ jackson-jq-extra/src/main/java/net/thisptr/jackson/jq/extra/functions/FromDateIso8601Function.java
@@ -1,10 +1,11 @@
 package net.thisptr.jackson.jq.extra.functions;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.JsonNodeType;
 import com.google.auto.service.AutoService;
 import net.thisptr.jackson.jq.*;
 import net.thisptr.jackson.jq.exception.JsonQueryException;
-import net.thisptr.jackson.jq.exception.JsonQueryTypeException;
+import net.thisptr.jackson.jq.extra.internal.misc.Preconditions;
 import net.thisptr.jackson.jq.internal.misc.JsonNodeUtils;
 import net.thisptr.jackson.jq.path.Path;
 
@@ -17,17 +18,12 @@ import java.util.List;
 public class FromDateIso8601Function implements Function {
     @Override
     public void apply(final Scope scope, final List<Expression> args, final JsonNode in, final Path ipath, final PathOutput output, final Version version) throws JsonQueryException {
-        if (in.isNumber()) {
-            output.emit(in, null);
-        } else if (in.isTextual()) {
-            try {
-                long epochSeconds = ZonedDateTime.parse(in.asText()).toEpochSecond();
-                output.emit(JsonNodeUtils.asNumericNode(epochSeconds), null);
-            } catch (DateTimeParseException e) {
-                throw new JsonQueryException(e);
-            }
-        } else {
-            throw new JsonQueryTypeException("%s cannot be parsed as an ISO 8601 timestamp", in);
+        Preconditions.checkInputType("fromdateiso8601", in, JsonNodeType.STRING);
+        try {
+            long epochSeconds = ZonedDateTime.parse(in.asText()).toEpochSecond();
+            output.emit(JsonNodeUtils.asNumericNode(epochSeconds), null);
+        } catch (DateTimeParseException e) {
+            throw new JsonQueryException(e);
         }
     }
 }
