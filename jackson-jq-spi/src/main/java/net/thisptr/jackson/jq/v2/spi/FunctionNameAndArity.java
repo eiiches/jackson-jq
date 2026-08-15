ad648740e3a263311cffdaa26d70a0a1f63c8769
Eiichi Sato <sato.eiichi@gmail.com>
2026-08-15 19:52:40 +0900

    add FunctionNameAndArity

---
+++ jackson-jq-spi/src/main/java/net/thisptr/jackson/jq/v2/spi/FunctionNameAndArity.java
@@ -0,0 +1,42 @@
+package net.thisptr.jackson.jq.v2.spi;
+
+import java.util.Objects;
+
+import org.jspecify.annotations.Nullable;
+
+public class FunctionNameAndArity {
+	private final String name;
+
+	// null arity means that the function is variadic
+	private final @Nullable Integer arity;
+
+	private FunctionNameAndArity(String name, @Nullable Integer arity) {
+		this.name = name;
+		this.arity = arity;
+	}
+
+	public static FunctionNameAndArity of(String name, int arity) {
+		// TODO: validate name here
+		return new FunctionNameAndArity(name, arity);
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (o == null || getClass() != o.getClass()) return false;
+		FunctionNameAndArity that = (FunctionNameAndArity) o;
+		return Objects.equals(name, that.name) && Objects.equals(arity, that.arity);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(name, arity);
+	}
+
+	public String toString() {
+		return name + (arity != null ? "/" + arity : "");
+	}
+
+	public FunctionNameAndArity withArity(@Nullable Integer arity) {
+		return new FunctionNameAndArity(name, arity);
+	}
+}
